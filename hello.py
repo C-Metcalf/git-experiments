296d5ee2407e5eb6406a55a0f373f14f83f823b8
Chris <Jarheadfro@gmail.com>
2024-10-23 14:23:17 -0400

    made func trunk

--- hello.py
+++ hello.py
@@ -1,6 +1,12 @@
 def main():
     print("Hello from gitexperiments!")
+    results = trunk()
+    print(results)
 
+def trunk():
+    x = 40
+    y = 20
+    return x * y
 
 if __name__ == "__main__":
     main()
