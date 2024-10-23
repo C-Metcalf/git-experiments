1561325a1b52788e241f08e0933ace7ae0559c81
Chris <Jarheadfro@gmail.com>
2024-10-23 14:50:16 -0400

    fixed rebase errors?

--- hello.py
+++ hello.py
@@ -9,30 +9,19 @@ def trunk():
     backend()
     return x * y
 
-<<<<<<< HEAD
-<<<<<<< HEAD
-def feature():
-    res = trunk()
-    new_res = res / 5 + 300
-    return new_res
-=======
-=======
->>>>>>> 1d4467e (made func feature)
 def backend():
     print("we do backend shiiii. you know how we do.")
->>>>>>> 10c77ac (Added backend func)
 
 def test():
     x =3
     y =2
     v = 3000
     print(x + y + v)
-=======
+    
 def feature():
     res = trunk()
     new_res = res / 5 + 300
     return new_res
->>>>>>> 74762ac (made func feature)
 
 if __name__ == "__main__":
     main()
