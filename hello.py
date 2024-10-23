ee47fc9040a99c33449270fc505bd497d3d8a71e
Chris <Jarheadfro@gmail.com>
2024-10-23 14:47:33 -0400

    no idea what is happening

--- hello.py
+++ hello.py
@@ -6,12 +6,18 @@ def main():
 def trunk():
     x = 40
     y = 20
+    backend()
     return x * y
 
+<<<<<<< HEAD
 def feature():
     res = trunk()
     new_res = res / 5 + 300
     return new_res
+=======
+def backend():
+    print("we do backend shiiii. you know how we do.")
+>>>>>>> 10c77ac (Added backend func)
 
 if __name__ == "__main__":
     main()
