74762ac10a4843ba70cb9cd400c78ba6dd70a29d
Chris <Jarheadfro@gmail.com>
2024-10-23 14:24:51 -0400

    made func feature

--- hello.py
+++ hello.py
@@ -1,12 +1,17 @@
 def main():
     print("Hello from gitexperiments!")
-    results = trunk()
+    results = feature()
     print(results)
 
 def trunk():
     x = 40
     y = 20
     return x * y
+
+def feature():
+    res = trunk()
+    new_res = res / 5 + 300
+    return new_res
 
 if __name__ == "__main__":
     main()
